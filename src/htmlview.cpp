371e3873e5f2633423c90ff177b81f6f4a31ad16
Klaas Freitag <freitag@kde.org>
2006-08-25 20:42:34 +0000

    Fixed header

--- src/htmlview.cpp
+++ src/htmlview.cpp
@@ -1,25 +1,18 @@
-/*
-    This file is part of Fate.
-
-    Copyright (c) 2005 SUSE LINUX Products GmbH
-
-    Author: Cornelius Schumacher <cschum@suse.de>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
-
+ /***************************************************************************
+                        htmlview.cpp  - show a html page
+                             -------------------
+    begin                : Aug 2006
+    copyright            : (C) 2006 Klaas Freitag <freitag@kde.org>
+                           (C) 2006 Cornelius Schumacher <schumacher@kde.org>
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 #include "htmlview.h"
 
 #include <klocale.h>
--- src/htmlview.h
+++ src/htmlview.h
@@ -1,30 +1,23 @@
-/*
-    This file is part of Fate.
+ /***************************************************************************
+                        htmlview.h  - show a html page
+                             -------------------
+    begin                : Aug 2006
+    copyright            : (C) 2006 Klaas Freitag <freitag@kde.org>
+                           (C) 2006 Cornelius Schumacher <schumacher@kde.org>
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
-    Copyright (c) 2005 SUSE LINUX Products GmbH
-
-    Author: Cornelius Schumacher <cschum@suse.de>
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-*/
 #ifndef HTMLVIEW_H
 #define HTMLVIEW_H
 
 #include <khtml_part.h>
-
-class MetaFile;
 
 class HtmlView : public KHTMLPart
 {
@@ -60,7 +53,7 @@ class HtmlView : public KHTMLPart
     void updateZoomActions();
     virtual bool loadCss();
   private:
-    MetaFile *mCssFile;
+
     QString mCss;
 
     QString mTitle;
