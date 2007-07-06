e7cc8b612707141626512945bcd9880356838a83
Klaas Freitag <freitag@kde.org>
2007-07-06 12:09:25 +0000

    - changes update - fixed sql migration to version 4

--- database/migration/4_dbmigrate.sql
+++ database/migration/4_dbmigrate.sql
@@ -11,3 +11,71 @@ CREATE TABLE DocTexts (
   PRIMARY KEY( docTextID ),
   INDEX( docType, textType )
 );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Offer', 'Header Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Offer', 'Footer Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Invoice', 'Header Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Invoice', 'Footer Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Acceptance of Order', 'Header Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Acceptance of Order', 'Footer Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Angebot', 'Header Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Angebot', 'Footer Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Rechnung', 'Header Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Rechnung', 'Footer Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Auftragsbestätigung', 'Header Text' );
+
+INSERT INTO DocTexts ( name, text, docType, textType ) 
+VALUES ( 'Standard', 'Please edit me - Bitte passe mich an!', 'Auftragsbestätigung', 'Footer Text' );
+
+
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Offer', 'Footer Text' FROM wordLists WHERE category='docFooter_Offer';
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Offer', 'Header Text' FROM wordLists WHERE category='docHeader_Offer';
+
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Invoice', 'Footer Text' FROM wordLists WHERE category='docFooter_Invoice';
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Invoice', 'Header Text' FROM wordLists WHERE category='docHeader_Invoice';
+
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Acceptance of Order', 'Footer Text' FROM wordLists WHERE category='docFooter_Acceptance of Order';
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Acceptance of Order', 'Header Text' FROM wordLists WHERE category='docHeader_Acceptance of Order';
+
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Angebot', 'Footer Text' FROM wordLists WHERE category='docFooter_Angebot';
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Angebot', 'Header Text' FROM wordLists WHERE category='docHeader_Angebot';
+
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Rechnung', 'Footer Text' FROM wordLists WHERE category='docFooter_Rechnung';
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Rechnung', 'Header Text' FROM wordLists WHERE category='docHeader_Rechnung';
+
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Auftragsbestätigung', 'Footer Text' FROM wordLists WHERE category='docFooter_Auftragsbestätigung';
+INSERT INTO DocTexts ( name, text, docType, textType )
+SELECT 'Standard', word, 'Auftragsbestätigung', 'Header Text' FROM wordLists WHERE category='docHeader_Auftragsbestätigung';
+
