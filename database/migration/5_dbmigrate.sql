02a5e3112fb185f4b4a844800648f5efcb28bd40
Klaas Freitag <freitag@kde.org>
2007-09-17 19:45:44 +0000

    more attribute changes for archdocpos

--- database/migration/5_dbmigrate.sql
+++ database/migration/5_dbmigrate.sql
@@ -8,8 +8,22 @@ CREATE TABLE attributes (
   PRIMARY KEY( hostObject, hostId)
 );
 
+# message Creating attributes for archived documents
+CREATE TABLE archdocAttribs (
+  archDocID  INT NOT NULL AUTO_INCREMENT,
+  name       VARCHAR(255),
+  value      VARCHAR(255),
+
+  PRIMARY KEY( archDocID )
+);
+
+# message Adding position type and overall price ot archdocpositions
 ALTER TABLE archdocpos ADD COLUMN kind VARCHAR(64) AFTER ordNumber;
 ALTER TABLE archdocpos ADD COLUMN overallPrice DECIMAL(10,2) AFTER price;
 
+# message Changing old kinds to Normal
 UPDATE archdocpos SET kind = "Normal";
+
+# message Calculating archive position price
 UPDATE archdocpos SET overallPrice = ROUND( price * amount, 2);
+
