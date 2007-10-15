07299adfbca02b117e4f1e96e36d5bcac889773c
Klaas Freitag <freitag@kde.org>
2007-10-15 20:26:23 +0000

    More refined doctype attributes

--- database/migration/5_dbmigrate.sql
+++ database/migration/5_dbmigrate.sql
@@ -1,11 +1,11 @@
 # message Creating attributes table...
 CREATE TABLE attributes (
   hostObject VARCHAR(255),
-  hostId     INT,
+  hostId     INT NOT NULL,
   name       VARCHAR(255),
   value      MEDIUMTEXT,
 
-  PRIMARY KEY( hostObject, hostId)
+  PRIMARY KEY( hostObject, hostId, name )
 );
 
 # message Creating attributes for archived documents
@@ -27,3 +27,23 @@ UPDATE archdocpos SET kind = "Normal";
 # message Calculating archive position price
 UPDATE archdocpos SET overallPrice = ROUND( price * amount, 2);
 
+# message Creating Document Type table
+CREATE TABLE DocTypes (
+  docTypeID INT NOT NULL AUTO_INCREMENT,
+  name      VARCHAR(255),
+  
+  PRIMARY KEY( docTypeID )
+);
+
+# message Filling doc type attributes
+INSERT INTO DocTypes (name) VALUES ( 'Offer' );
+SET @dtId := LAST_INSERT_ID();
+INSERT INTO attributes VALUES ('DocType', @dtId, 'AllowDemand', 'true');
+INSERT INTO attributes VALUES ('DocType', @dtId, 'AllowAlternative', 'true');
+
+INSERT INTO DocTypes (name) VALUES ( 'Acceptance of Order' );
+SET @dtId := LAST_INSERT_ID();
+INSERT INTO attributes VALUES ('DocType', @dtId, 'AllowDemand', 'true');
+INSERT INTO attributes VALUES ('DocType', @dtId, 'AllowAlternative', 'true');
+
+INSERT INTO DocTypes (name) VALUES ( 'Invoice' );
