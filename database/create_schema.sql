adf2e01254e4064d1ea97399cec655edd049cf6e
Klaas Freitag <freitag@kde.org>
2007-11-21 21:32:46 +0000

    removed docDesc creation statement

--- database/create_schema.sql
+++ database/create_schema.sql
@@ -148,7 +148,6 @@ CREATE TABLE document(
     docID             INT NOT NULL AUTO_INCREMENT,
     ident             VARCHAR(32),
     docType           VARCHAR(255),
-    docDescription    TEXT,
     clientID          VARCHAR(32),
     clientAddress     TEXT,
     salut             VARCHAR(255),
