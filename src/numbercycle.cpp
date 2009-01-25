8928821dcf37d1ad9642b07b3c1837bfdc70659c
Klaas Freitag <freitag@kde.org>
2009-01-25 20:44:20 +0000

    missing files added

---
+++ src/numbercycle.cpp
@@ -0,0 +1,58 @@
+/***************************************************************************
+                   numbercycle.h  - document number cycles
+                             -------------------
+    begin                : Jan 15 2009
+    copyright            : (C) 2009 by Klaas Freitag
+    email                : freitag@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "numbercycle.h"
+
+NumberCycle::NumberCycle()
+{
+
+}
+
+NumberCycle::NumberCycle( dbID _id )
+  :id( _id )
+{
+}
+
+void NumberCycle::setName( const QString& n )
+{
+  mName = n;
+}
+
+QString NumberCycle::name()
+{
+  return mName;
+}
+
+void NumberCycle::setTemplate( const QString& t )
+{
+  mTemplate = t;
+}
+
+QString NumberCycle::getTemplate()
+{
+  return mTemplate;
+}
+
+void NumberCycle::setCounter( int c )
+{
+  mCounter = c;
+}
+
+int  NumberCycle::counter()
+{
+  return mCounter;
+}
---
+++ src/numbercycle.h
@@ -0,0 +1,46 @@
+/***************************************************************************
+                   numbercycle.h  - document number cycles
+                             -------------------
+    begin                : Jan 15 2009
+    copyright            : (C) 2009 by Klaas Freitag
+    email                : freitag@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef NUMBERCYCLE_H
+#define NUMBERCYCLE_H
+
+#include <qstring.h>
+
+#include "dbids.h"
+
+class NumberCycle
+{
+public:
+  NumberCycle();
+  NumberCycle( dbID );
+
+  void setName( const QString& );
+  QString name();
+
+  void setTemplate( const QString& );
+  QString getTemplate();
+
+  void setCounter( int );
+  int  counter();
+private:
+  dbID id;
+  QString mName;
+  QString mTemplate;
+  int     mCounter;
+};
+
+#endif
