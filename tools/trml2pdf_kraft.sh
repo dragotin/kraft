01b8211c8a8d4e73e17ea5c6eae2aef0d694304a
Klaas Freitag <freitag@kde.org>
2009-03-08 15:50:51 +0000

    rml2pdf merge script

---
+++ tools/trml2pdf_kraft.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+
+#/***************************************************************************
+# trml2pdf_kraft.sh - script to render the pdf files for kraft
+#                             -------------------
+#    begin                : Feb 2009
+#    copyright            : (C) 2009 by Andreas Wuest
+#                                    original version by Klaas Freitag                         
+#    email                : andreaswuest@gmx.de
+# ***************************************************************************/
+
+#/***************************************************************************
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU General Public License as published by  *
+# *   the Free Software Foundation; either version 2 of the License, or     *
+# *   (at your option) any later version.                                   *
+# *                                                                         *
+# ***************************************************************************/
+
+# uncomment for debug messages
+# set -x
+
+tmpDir="/tmp/kraft"
+
+# print the usage
+usage() {
+  echo "Usage: $0 {0|1|2} trml-file watermark-pdf-file"
+}
+
+# check the infile exists
+checkInfile() {
+  if [ ! -e $1 ]; then
+    echo "Trml file \"$1\" does not exist!"
+    usage
+    exit 1;
+  fi
+}
+
+# check if watermark exists
+checkWatermarkFile() {
+  if [ -z $1 ]; then
+    echo "Watermark file \"$1\" does not exist!"
+    exit 1;
+  fi
+
+  if [ ! -e $1 ]; then
+    echo "Watermark file \"$1\" does not exist!"
+    usage
+    exit 1;
+  fi
+}
+
+# we need at least 2 arguments (or 3 for watermark)
+if [ "$#" -lt 2 ]; then
+  usage
+  exit 1;
+fi
+
+# cleanup the resources and create new directory
+if [ -d $tmpDir ]; then
+  rm -rf $tmpDir
+fi
+
+mkdir $tmpDir
+
+type=$1
+infile=$2
+
+case "$type" in
+  0)
+     echo "no watermark"
+     checkInfile $infile
+     # render the document
+     trml2pdf $infile > $tmpDir/rendered.pdf
+     # print pdf to stdout
+     pdftk $tmpDir/rendered.pdf cat output -
+     ;;
+  1) 
+     echo "watermark on first page"
+     watermark=$3
+     checkInfile $infile
+     checkWatermarkFile $watermark
+     # render the document
+     trml2pdf $infile > $tmpDir/rendered.pdf
+     # burst the document
+     pdftk $tmpDir/rendered.pdf burst
+     # apply the watermark
+     pdftk pg_0001.pdf background $watermark output $tmpDir/firstback.pdf
+     # remove the tmp file
+     rm pg_0001.pdf
+     # merge all together
+     if [ -e pg_0002.pdf ]; then
+       pdftk $tmpDir/firstback.pdf pg_*.pdf cat output -
+     else
+       pdftk $tmpDir/firstback.pdf cat output -
+     fi
+     ;;
+  2)
+     echo "watermark on all pages"
+     watermark=$3
+     checkInfile $infile
+     checkWatermarkFile $watermark
+     # render the document
+     trml2pdf $infile > $tmpDir/rendered.pdf
+     # apply the watermark
+     pdftk $tmpDir/rendered.pdf background $watermark output $tmpDir/renderedFinal.pdf
+     # print pdf to stdout
+     pdftk $tmpDir/renderedFinal.pdf cat output -
+     ;;
+  *) 
+     usage
+     exit 1
+     ;;
+esac
+
+exit 0;
+
