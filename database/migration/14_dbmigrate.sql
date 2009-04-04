24dcd051d3201a8f2c85cca1eb49d0ef38bbbf08
Klaas Freitag <freitag@kde.org>
2009-04-04 12:25:15 +0000

    Correct german VAT

--- database/migration/14_dbmigrate.sql
+++ database/migration/14_dbmigrate.sql
@@ -8,5 +8,5 @@ CREATE TABLE taxes (
 
     PRIMARY KEY( id )
 );
-
-INSERT INTO taxes ( fullTax, reducedTax, startDate ) VALUES (19.0, 7.0, '2008-01-01' );
+INSERT INTO taxes ( fullTax, reducedTax, startDate ) VALUES (16.0, 7.0, '1998-04-01' );
+INSERT INTO taxes ( fullTax, reducedTax, startDate ) VALUES (19.0, 7.0, '2007-01-01' );
