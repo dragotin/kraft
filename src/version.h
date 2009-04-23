48c0e00e36674d8c58c8b4bad6ff1b8b2e3e474b
Klaas Freitag <freitag@kde.org>
2009-04-23 13:01:46 +0000

    Version information updated

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define KPLANT_VERSION "0.4"
-#define KRAFT_VERSION "0.32beta3"
+#define KRAFT_VERSION "0.32beta4"
 
-#define KRAFT_CODENAME "Proper Baby3"
+#define KRAFT_CODENAME "Toddler"
 
 #define KRAFT_REQUIRED_SCHEMA_VERSION 15
