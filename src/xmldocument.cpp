9b40ffdf9be46f80288c10d0b3f4d7f57ab226d3
Klaas Freitag <freitag@kde.org>
2013-02-17 21:46:59 +0200

    Added proper license header

--- src/xmldocument.cpp
+++ src/xmldocument.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+             xmldocument - XML document writer, kode based.
+                             -------------------
+    begin                : 2013-02-15
+    copyright            : (C) 2013 by Klaas Freitag
+    email                : freitag@kde.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include <QDate>
 
