10824488344880c70a2e47b9c248f4d0b6d9acfc
Klaas Freitag <freitag@kde.org>
2013-02-26 11:25:22 +0100

    Added license header.

--- src/owncloudsync.cpp
+++ src/owncloudsync.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+ *                    owncloudync - sync to ownCloud 
+ *                            -------------------
+ *    begin                : feb 20, 2013
+ *    copyright            : (C) 2013 by Klaas Freitag
+ *    email                : freitag@kde.org
+ ****************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ****************************************************************************/
 #include "owncloudsync.h"
 #include "mirall/owncloudfolder.h"
 #include "mirall/credentialstore.h"
