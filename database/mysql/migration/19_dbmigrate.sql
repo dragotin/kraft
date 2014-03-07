e7930e88ed3253921889416e61feaa7d130d1c69
Klaas Freitag <freitag@kde.org>
2014-03-07 22:01:15 +0100

    Add document relations to database migration for Delivery Receipt.

--- database/mysql/migration/19_dbmigrate.sql
+++ database/mysql/migration/19_dbmigrate.sql
@@ -3,7 +3,7 @@ INSERT INTO DocTypes (name) VALUES ('Lieferschein');
 SET @lsId := LAST_INSERT_ID();
 INSERT INTO attributes (hostObject, hostId, name, valueIsList) VALUES ('DocType', @lsId, 'HidePrices', 1);
 
-INSERT INTO DocTypes (name) VALUES ('Note of Delivery');
+INSERT INTO DocTypes (name) VALUES ('Delivery Receipt');
 SET @nodId := LAST_INSERT_ID();
 INSERT INTO attributes (hostObject, hostId, name, valueIsList) VALUES ('DocType', @nodId, 'HidePrices', 1);
 
@@ -14,6 +14,15 @@ INSERT INTO DocTypeRelations VALUES( @item, @lsId, 10 );
 # Rechnung follows Lieferschein
 SELECT @follower := docTypeID FROM DocTypes WHERE name="Rechnung";
 INSERT INTO DocTypeRelations VALUES( @lsId, @follower, 11 );
+
+# Delivery Receipt follows Offer
+SELECT @item := docTypeID FROM DocTypes WHERE name="Offer";
+INSERT INTO DocTypeRelations VALUES( @item, @nodId, 12 );
+
+# Invoice follows Delivery Receipt
+SELECT @follower := docTypeID FROM DocTypes WHERE name="Invoice";
+INSERT INTO DocTypeRelations VALUES( @nodId, @follower, 13 );
+
 
 # enhance the clientID col in document because the ids can be larger.
 ALTER TABLE document CHANGE COLUMN clientID clientID VARCHAR(255);
--- database/sqlite3/migration/19_dbmigrate.sql
+++ database/sqlite3/migration/19_dbmigrate.sql
@@ -3,9 +3,16 @@ INSERT INTO DocTypes (name) VALUES ('Lieferschein');
 INSERT INTO attributes (hostObject, hostId, name, valueIsList) VALUES ('DocType', 
 (SELECT docTypeID FROM DocTypes WHERE name="Lieferschein"), 'HidePrices', 'true');
 
-INSERT INTO DocTypes (name) VALUES ('Note of Delivery');
+INSERT INTO DocTypes (name) VALUES ('Delivery Receipt');
 INSERT INTO attributes (hostObject, hostId, name, valueIsList) VALUES ('DocType', 
-(SELECT docTypeID FROM DocTypes WHERE name="Note of Delivery"), 'HidePrices', 'true');
+(SELECT docTypeID FROM DocTypes WHERE name="Delivery Receipt"), 'HidePrices', 'true');
+
+# Delivery Receipt follows Offer
+INSERT INTO DocTypeRelations VALUES( (SELECT docTypeID FROM DocTypes WHERE name="Offer"),
+	(SELECT docTypeID FROM DocTypes WHERE name="Delivery Receipt"), 12 );
+
+INSERT INTO DocTypeRelations VALUES( (SELECT docTypeID FROM DocTypes WHERE name="Delivery Receipt"),
+	(SELECT docTypeID FROM DocTypes WHERE name="Invoice"), 13 );
 
 # Lieferschein follows Angebot
 INSERT INTO DocTypeRelations VALUES( (SELECT docTypeID FROM DocTypes WHERE name="Angebot"),
