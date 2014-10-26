c8d74332da014a1c7a14bd44a55044d17fc74266
Klaas Freitag <freitag@kde.org>
2014-10-26 18:59:16 +0100

    Let KraftDocFooterEdit class handle the salut combobox.
    Fix handling of custom entries in the combo box. Custom entries are now
    added as extra combo box items.

--- src/kraftdocfooteredit.cpp
+++ src/kraftdocfooteredit.cpp
@@ -16,9 +16,11 @@
  ***************************************************************************/
 
 #include "kraftdocfooteredit.h"
+#include "kraftdb.h"
 
 #include <kcombobox.h>
 #include <klocale.h>
+#include <kdebug.h>
 
 #include <QLayout>
 #include <QComboBox>
@@ -26,7 +28,8 @@
 
 KraftDocFooterEdit::KraftDocFooterEdit( QWidget *parent )
   : KraftDocEdit( parent ),
-  mDocFooterEdit( 0 )
+  mDocFooterEdit( 0 ),
+  mCustomGreetingIndex(-1)
 {
   QVBoxLayout *topLayout = new QVBoxLayout;
   Q_ASSERT( parent );
@@ -37,11 +40,56 @@ KraftDocFooterEdit::KraftDocFooterEdit( QWidget *parent )
   mDocFooterEdit->setupUi(w);
   topLayout->addWidget(w);
 
+  mDocFooterEdit->m_cbGreeting->insertItems(-1, KraftDB::self()->wordList( "greeting" ) );
+
   connect( mDocFooterEdit->m_cbGreeting, SIGNAL( activated( int ) ),
-    SLOT( slotModified() ) );
+           SLOT( slotModified() ) );
+  connect( mDocFooterEdit->m_cbGreeting, SIGNAL(currentIndexChanged(int)),
+           this, SLOT(slotGreeterIndexChanged(int)));
+  connect( mDocFooterEdit->m_cbGreeting, SIGNAL(editTextChanged(QString)),
+           this, SLOT(slotGreeterEditTextChanged(QString)));
   connect( mDocFooterEdit->m_teSummary, SIGNAL( textChanged() ),
-    SLOT( slotModified() ) );
+           SLOT( slotModified() ) );
 
   setTitle( i18n( "Document Footer" ) );
   setColor( "#f0ff9a" );
+}
+
+void KraftDocFooterEdit::slotSetGreeting( const QString& newText )
+{
+    slotGreeterEditTextChanged(newText);
+}
+
+QString KraftDocFooterEdit::greeting()
+{
+    return mGreeting;
+}
+
+void KraftDocFooterEdit::slotGreeterIndexChanged(int newIndex)
+{
+    mGreeting = mDocFooterEdit->m_cbGreeting->itemText(newIndex);
+    slotModified();
+}
+
+void KraftDocFooterEdit::slotGreeterEditTextChanged(const QString& newText)
+{
+    QComboBox *greeterCombo = qobject_cast<QComboBox*>(mDocFooterEdit->m_cbGreeting);
+    if( !greeterCombo ) return;
+    kDebug() << "II Combo box text changed to" << newText << "in" << greeterCombo;
+
+    const QStringList texts = KraftDB::self()->wordList("greeting");
+    int indx = greeterCombo->currentIndex();
+    if( !texts.contains(newText)) {
+        if( mCustomGreetingIndex == -1 ) {
+            // no custom Entry yet
+            greeterCombo->insertItem(0, newText);
+            mCustomGreetingIndex = 0;
+        }
+        indx = mCustomGreetingIndex;
+    }
+    greeterCombo->setItemText(indx, newText);
+    greeterCombo->setCurrentIndex(indx);
+
+    mGreeting = newText;
+    slotModified();
 }
