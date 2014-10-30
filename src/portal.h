9223feaa88af52768a479c8a2193a26e2339bdec
Klaas Freitag <freitag@kde.org>
2014-10-30 10:22:49 +0100

    Handle user input per link to set a document in sent state.

--- src/portal.h
+++ src/portal.h
@@ -154,6 +154,7 @@ class Portal : public KXmlGuiWindow
     void slotMailPdfAvailable( const QString& fileName );
     void slotMailAddresseeFound( const QString&, const KABC::Addressee& );
 
+    void slotMarkArchivedDocSent( const ArchDocDigest& );
   private:
     void createView( DocGuardedPtr );
     void createROView( DocGuardedPtr );
