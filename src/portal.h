4c6166c8c4d7e6f1f90b3a1f418589bf0e3cf3c8
Klaas Freitag <freitag@kde.org>
2015-01-16 08:47:22 +0100

    Set a sent date for archived documents.

--- src/portal.h
+++ src/portal.h
@@ -154,7 +154,6 @@ class Portal : public KXmlGuiWindow
     void slotMailPdfAvailable( const QString& fileName );
     void slotMailAddresseeFound( const QString&, const KABC::Addressee& );
 
-    void slotMarkArchivedDocSent( const ArchDocDigest& );
   private:
     void createView( DocGuardedPtr );
     void createROView( DocGuardedPtr );
