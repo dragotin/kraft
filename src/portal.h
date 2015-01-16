ddbecb03c869a15c3c721d8328f45c86986d1f25
Klaas Freitag <freitag@kde.org>
2015-01-16 18:35:58 +0100

    Fix compiling again.

--- src/portal.h
+++ src/portal.h
@@ -151,9 +151,6 @@ class Portal : public KXmlGuiWindow
 
     void slotMarkArchivedDocSent( const ArchDocDigest& );
 
-    void slotMailPdfAvailable( const QString& fileName );
-    void slotMailAddresseeFound( const QString&, const KABC::Addressee& );
-
   private:
     void createView( DocGuardedPtr );
     void createROView( DocGuardedPtr );
