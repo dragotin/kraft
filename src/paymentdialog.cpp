596d8475b25501d4435cf795da313e058961811b
Klaas Freitag <freitag@kde.org>
2015-03-08 13:57:30 +0100

    Start of paymentdialog class

---
+++ src/paymentdialog.cpp
@@ -0,0 +1,12 @@
+#include "paymentdialog.h"
+
+PaymentDialog::PaymentDialog()
+{
+
+}
+
+PaymentDialog::~PaymentDialog()
+{
+
+}
+
---
+++ src/paymentdialog.h
@@ -0,0 +1,13 @@
+#ifndef PAYMENTDIALOG_H
+#define PAYMENTDIALOG_H
+
+#include <QWidget>
+
+class PaymentDialog : public KDialog
+{
+public:
+    PaymentDialog();
+    ~PaymentDialog();
+};
+
+#endif // PAYMENTDIALOG_H
