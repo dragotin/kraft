85b181c2c2cbae5a695b23804d57f510614df289
andy <andy@linux-imgj.suse>
2018-02-05 21:59:04 +0100

    more i18n polishing

--- src/docdigestdetailview.cpp
+++ src/docdigestdetailview.cpp
@@ -418,13 +418,17 @@ void DocDigestDetailView::slotShowDocDetails( DocDigest digest )
     if( archDocs.isEmpty() ) {
         // qDebug () << "No archived docs for this document!";
         tmpl.createDictionary( DOCDIGEST_TAG( "NEVER_PRINTED" ));
-        tmpl.setValue( "NEVER_PRINTED", DOCDIGEST_TAG("ARCHDOCS_TAG"), i18n("This document was never printed."));
+        tmpl.setValue( "NEVER_PRINTED", "NEVER_PRINTED_LABEL", i18n("This document was never printed."));
     } else {
         ArchDocDigest digest = archDocs[0];
         tmpl.createDictionary("PRINTED");
+        tmpl.setValue( "PRINTED", "LAST_PRINT_LABEL", i18n( "Last printed" ) );
+        tmpl.setValue( "PRINTED", "LAST_PRINT_TITLE", i18n( "Opens last created PDF document" ) );
+        tmpl.setValue( "PRINTED", "LAST_PRINT_LINK_TEXT", i18n( "open" ) );
         tmpl.setValue( "PRINTED", DOCDIGEST_TAG("LAST_PRINT_DATE"), digest.printDate().toString() );
         tmpl.setValue( "PRINTED", DOCDIGEST_TAG("LAST_PRINTED_ID"), digest.archDocId().toString() );
-        tmpl.setValue( "PRINTED", DOCDIGEST_TAG("ARCHIVED_COUNT"), QString::number( archDocs.count()-1 ) );
+        const QString archivedCount = (archDocs.size() == 1 ? i18n( "%1 older print" ) : i18n( "%1 older prints" ));
+        tmpl.setValue( "PRINTED", DOCDIGEST_TAG("ARCHIVED_COUNT"), archivedCount.arg( QString::number( archDocs.count()-1 )) );
     }
 
     const QString details = tmpl.expand();
