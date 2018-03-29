5d7d70acfa2544f14c781c892f870f4004bf8756
Klaas Freitag <freitag@kde.org>
2018-03-29 21:54:42 +0200

    Fix the translations extract script.

--- src/Messages.sh
+++ src/Messages.sh
@@ -1,3 +1,15 @@
 #!bin/sh
+
+EXTRACTRC=extractrc
+XGETTEXT=xgettext
+
+if [ -z "$KRAFT_HOME" ]; then
+    echo "Need to set KRAFT_HOME"
+    exit 1
+fi 
+
+podir=$KRAFT_HOME/po
+echo "PODir: " $podir
+pushd $KRAFT_HOME/src
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kraft.pot
+$XGETTEXT --keyword=i18n --language=C++ --from-code=UTF-8 `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/kraft.pot
