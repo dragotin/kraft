9af9dd92e3305a4a39d5c77c54a76a8d4af5b5f8
Klaas Freitag <kraft@freisturz.de>
2022-10-22 22:27:46 +0200

    Improve the attribute test

--- tests/t_attributes.cpp
+++ tests/t_attributes.cpp
@@ -6,6 +6,7 @@
 #include "testconfig.h"
 #include "kraftdb.h"
 #include "attribute.h"
+#include "dbids.h"
 
 void init_test_db()
 {
@@ -73,11 +74,60 @@ private slots:
     }
 
     void saveAndLoadAttribs() {
-        AttributeMap map;
+        const QString host {"testhost"};
+        AttributeMap map(host);
+        Attribute att1 = makeAtt("Test", "foo");
+        Attribute att2 = makeAtt("Test2", "foobar");
+        map[att1.name()] = att1;
+        map[att2.name()] = att2;
 
 
+        dbID id(342);
+        map.save(id);
+
+        AttributeMap m2(host);
+        m2.load(id);
+
+        QVERIFY(m2.contains(att1.name()));
+        QVERIFY(m2.contains(att2.name()));
+    }
+
+    void listValues() {
+        const QString host {"listtest"};
+        AttributeMap map(host);
+        {
+            Attribute att1 = makeAtt("Test", "foo");
+            att1.setListValue(true);
+            const QStringList li1 {"foo", "bar", "baz"};
+            att1.setValue(QVariant(li1));
+            map[att1.name()] = att1;
+        }
+        Attribute att2 = makeAtt("Test2", "foobar");
+        att2.setListValue(true);
+        const QStringList li2 {"foo", "bar", "baz"};
+        att2.setValue(QVariant(li2));
+        map[att2.name()] = att2;
+
+        // --- save
+        dbID id(344);
+        map.save(id);
+
+        // --- and load again
+        AttributeMap m2(host);
+        m2.load(id);
+
+        QVERIFY(m2.contains("Test"));
+        QVERIFY(m2.contains(att2.name()));
+
+        Attribute a3 = m2["Test2"];
+        const QStringList li = a3.value().toStringList();
+        QVERIFY(li.contains("foo"));
+        QVERIFY(li.contains("bar"));
+        QVERIFY(li.contains("baz"));
 
     }
+
+
 private:
     Attribute makeAtt(const QString& name, const QString& val) {
         Attribute att(name);
