454c4cdf94d32bbe267066974035a5a6c3c90923
Klaas Freitag <kraft@freisturz.de>
2023-04-07 18:33:49 +0200

    Remove database id from the kraftdoc

--- src/kraftview.cpp
+++ src/kraftview.cpp
@@ -1443,7 +1443,7 @@ void KraftView::discardChanges()
   if( doc && doc->modified() ) {
       bool ok = DocumentMan::self()->reloadDocument(doc);
       if (!ok) {
-          qDebug() << "Failed to reload doc" << doc->docIdStr();
+          qDebug() << "Failed to reload doc" << doc->docIdentifier();
       }
     // qDebug () << "Document refetch from database" << endl;
   }
